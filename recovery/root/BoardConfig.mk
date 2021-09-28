bbb14686b3528e3c5cc6743d6eb340d48efb8358
gokur20 <gokur20@hotmail.com>
2021-09-28 11:30:15 +0200

    Add files via upload

---
+++ recovery/root/BoardConfig.mk
@@ -0,0 +1,98 @@
+LOCAL_PATH := device/Xiaomi/Mi_Play
+
+TARGET_BOARD_PLATFORM := mt6765               # From ro.mediatek.platform, but lowercase value
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := lotus     # From ro.product.board
+
+# These two are for MTK Chipsets only
+BOARD_USES_MTK_HARDWARE := true
+BOARD_HAS_MTK_HARDWARE := true
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true            # To add info about F2FS Filesystem Data Block
+# Put The Size of your Recovery Partition below, get it from your "MT****_Android_scatter.txt"
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432 # recovery.img-origsize says 33554432 Scatter says 0x2000000 in Hex
+# BOARD_USES_FULL_RECOVERY_IMAGE := true      # Uncomment this line if you want to remove size restriction
+BOARD_FLASH_BLOCK_SIZE := 0                   # Might be different for your chip
+BOARD_HAS_NO_REAL_SDCARD := true              # Depricated
+# BOARD_HAS_NO_SELECT_BUTTON := true          # Depricated
+BOARD_SUPPRESS_SECURE_ERASE := true
+#BOARD_HAS_NO_MISC_PARTITION := true           # Delete if your partition table has /misc
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+RECOVERY_SDCARD_ON_DATA := true               # Optional: If /sdcard partition is emulated on /data partition 
+
+# TWRP stuff
+TW_EXCLUDE_SUPERSU := true                    # true/false: Add SuperSU or not
+TW_INCLUDE_CRYPTO := true                     # true/false: Add Data Encryption Support or not
+TW_INPUT_BLACKLIST := "hbtp_vm"               # Optional: Disables virtual mouse
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME := portrait_hdpi                     # Set the exact theme you wanna use. If resulation doesn't match, define the height/width
+DEVICE_RESOLUTION := 1080x2280                 # The Resolution of your Device
+TARGET_SCREEN_HEIGHT := 2280                    # The height
+TARGET_SCREEN_WIDTH := 1080                      # The width
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+# Set the Brightness Control File Path below (as per your chip/device)
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+# Set the Path of Logical Units (LUNs) for Storage below (as per your chip/device)
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 80                   # Set custom brightness, low is better
+
+TW_INCLUDE_NTFS_3G := true                    # Include NTFS Filesystem Support
+TW_INCLUDE_FUSE_EXFAT := true                 # Include Fuse-ExFAT Filesystem Support
+TWRP_INCLUDE_LOGCAT := true                   # Include LogCat Binary
+TW_INCLUDE_FB2PNG := true                     # Include Screenshot Support
+TW_DEFAULT_LANGUAGE := it                     # Set Default Language 
+TW_EXTRA_LANGUAGES := false
+
+# Kernel
+TARGET_IS_64_BIT := true                      # true/false: Determine if the device is 64-bit or not
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
+TARGET_PREBUILT_RECOVERY_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
+# Get the CMDLine, Base, Pagesize and offsets from Unpacked recovery image and put below
+#FROM ORIGINAL recovery.img-cmdline
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive androidboot.configfs=true
+#FROM ORIGINAL recovery.img-kernel_offset
+BOARD_KERNEL_BASE := 0x00008000
+#FROM ORIGINAL recovery.img-pagesize
+BOARD_KERNEL_PAGESIZE := 2048
+#Values from recovery.img-ramdisk_offset and recovery.img-tags_offset
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x14f88000 --tags_offset 0x13f88000
+
+# Set FSTAB
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/recovery.fstab
+
+TARGET_BOARD_SUFFIX := _64                    # Remove if the device is 32-bit
+TARGET_USES_64_BIT_BINDER := true             # Remove if the device is 32-bit
+
+# Architecture
+# According to the device's architecture (64-bit or 32-bit)
+#cìè da fare qualcosa??
+ifeq ($(TARGET_IS_64_BIT),true)
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
+TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
+TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
+else
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI),$(TARGET_CPU_ABI2)
+endif

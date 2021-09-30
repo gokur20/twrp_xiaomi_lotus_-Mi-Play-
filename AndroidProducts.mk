3c2c2a3faaf719a335d7d030384805d4a7d630d4
gokur20 <gokur20@hotmail.com>
2021-09-30 10:08:36 +0200

    Delete AndroidProducts.mk

--- AndroidProducts.mk
+++
@@ -1,9 +0,0 @@
-    # Replace $$DEVICE$$ with your Device Name's Value.
-    # Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
-    # The part of last line in mine looks like "omni_Primo_RX5.mk"
-
-    LOCAL_PATH := device/Xiaomi/Mi_Play
-
-    PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_Mi_Play.mk
-
-
